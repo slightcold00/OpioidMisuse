cdf986b76fb29d88e0639ee184950e399c6b2cc5
slightcold00 <hannahhu97@gmail.com>
2018-07-18 04:36:57 +0800

    updata

---
+++ myplot.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
+ 
+name_list = ['codeine','fentanyl','hydrocodone','norco','oxycodone','oxycontin','percocet','vicodin']
+num_list = [11581,20783,466,1078,1053,3558,4351,1600]
+plt.barh(range(len(num_list)), num_list,tick_label = name_list)
+plt.ylabel("kind of opioid")
+plt.xlabel("number of related tweets")
+plt.show()
+
+name_list = ['codeine','fentanyl','oxycontin','percocet']
+num_list = [5401,6091,1245,1358]
+plt.barh(range(len(num_list)), num_list,tick_label = name_list)
+plt.ylabel("kind of opioid")
+plt.xlabel("number of related tweets(no RT)")
+plt.show()
--- predeal.py
+++ predeal.py
@@ -17,14 +17,19 @@ model = gensim.models.Word2Vec.load('web_words.model')
 #将分词结果转化为向量
 def doc2vec(document):
     # 100维的向量
-    vec = np.zeros(100)
+    dec = np.zeros(100)
+    word_vec = np.zeros(100)
+    num = 0
 
     for word in document:
         try:
-            vec = model[word]
+            word_vec = model[word]
+            if word_vec != np.zeros(100):
+                dec += word_vec
+                num += 1
         except:
             continue
-
+    vec = dec/num        #对基于word2vec的词向量家和求平均
     return vec
 
 for row in f_csv:
